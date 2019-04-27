24532adb6330e560be4ed5e26cf25ce45d4a7efa
kramnani <kramnani@IN-MUM-LNDOT032>
2019-04-27 17:12:54 +0530

    Changes Overall

---
+++ UpsellManagementSystem/Controllers/ProductRecommendController.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using UpsellManagementSystem.Models;
+
+namespace UpsellManagementSystem.Controllers
+{
+    public class ProductRecommendController : Controller
+    {
+        DbEntities _products = new DbEntities();
+
+        // GET: ProductRecommend
+        public ActionResult Index(ProductsRecommend_174866_MiniProj recommendProd)
+        {
+            var displayAllItems = _products.ProductsRecommend_174866_MiniProj.ToList();
+
+            if (displayAllItems != null)
+            {
+                return View(displayAllItems);
+            }
+
+            return View();
+        }
+    }
+}
