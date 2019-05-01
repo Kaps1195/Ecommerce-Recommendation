4bb0d450b514cb9d95b767d8fdff576aa3ff1450
kaps1195 <kramnani95@gmail.com>
2019-05-01 23:58:01 +0530

    Added Route for Admin

--- UpsellManagementSystem/App_Start/RouteConfig.cs
+++ UpsellManagementSystem/App_Start/RouteConfig.cs
@@ -16,7 +16,8 @@ namespace UpsellManagementSystem
             routes.MapRoute(
                 name: "Default",
                 url: "{controller}/{action}/{id}",
-                defaults: new { controller = "Login", action = "Login", id = UrlParameter.Optional }
+                defaults: new { controller = "Login", action = "Login", id = UrlParameter.Optional },
+                namespaces: new[] { "UpsellManagementSystem.Controllers" }
             );
         }
     }
