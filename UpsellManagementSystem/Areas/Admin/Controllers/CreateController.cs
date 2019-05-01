fe81ba6eafaa6ec188caf278fc7b63efd9a960de
kaps1195 <kramnani95@gmail.com>
2019-05-02 00:05:56 +0530

    Create Controller To Add category and products

---
+++ UpsellManagementSystem/Areas/Admin/Controllers/CreateController.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using UpsellManagementSystem.Models;
+
+namespace UpsellManagementSystem.Areas.Admin.Controllers
+{
+    public class CreateController : Controller
+    {
+        ProjectEntitiesUpdated _product = new ProjectEntitiesUpdated();
+
+        public ActionResult AddCategory()
+        {
+            return View();
+        }
+
+        // GET: Admin/Create/AddCategory
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult AddCategory(Categories_174866_MiniProj newCategory)
+        {
+            _product.Categories_174866_MiniProj.Add(newCategory);
+            _product.SaveChanges();
+            return View(newCategory);
+        }
+
+        public ActionResult AddProduct()
+        {
+            return View();
+        }
+
+        // GET: Admin/Create/AddProduct
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult AddProduct(Products_174866_MiniProj newProduct)
+        {
+            _product.Products_174866_MiniProj.Add(newProduct);
+            _product.SaveChanges();
+            return View(newProduct);
+        }
+    }
+}
