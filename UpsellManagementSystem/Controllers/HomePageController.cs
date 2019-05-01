b8c0910ad1e9655ce5dec38bc43581b0a2bd7034
kaps1195 <kramnani95@gmail.com>
2019-05-02 00:03:12 +0530

    Passing controller data via TempData

--- UpsellManagementSystem/Controllers/HomePageController.cs
+++ UpsellManagementSystem/Controllers/HomePageController.cs
@@ -11,27 +11,42 @@ namespace UpsellManagementSystem.Controllers
     {
         ProjectEntitiesUpdated _products = new ProjectEntitiesUpdated();
         // GET: HomePage
-        public ActionResult Index(Products_174866_MiniProj products)
+        public ActionResult Index()
         {
-            
+
             var displayAllItems = _products.Products_174866_MiniProj.ToList();
 
             if (displayAllItems != null)
             {
                 return View(displayAllItems);
             }
-            
+
             return View();
         }
 
         [HttpPost]
+        [ValidateAntiForgeryToken]
         public ActionResult Index(string ModelName)
         {
             var searchModelName = from k in _products.Products_174866_MiniProj.ToList()
-                                       where k.ModelName == ModelName
-                                       select k;
+                                  where k.ModelName == ModelName
+                                  select k;
             return View(searchModelName);
         }
-        
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Index(Products_174866_MiniProj submitProduct)
+        {
+            //string value = Convert.ToString(data["submitProduct"]);
+
+            TempData["userSelectedProduct"] = submitProduct;
+
+            //return RedirectToAction("ProductDetail", "ProductDetails", new { selectedProd = submitProduct });
+
+            return RedirectToAction("ProductDetail", "ProductDetails");
+
+        }
+
     }
 }
