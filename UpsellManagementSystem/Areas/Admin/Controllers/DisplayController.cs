9f0022dde6c8588e67b3fbbfc15046a6a08b9976
kramnani <kramnani@IN-MUM-LNDOT032>
2019-05-02 18:12:03 +0530

    Added Display Controller

---
+++ UpsellManagementSystem/Areas/Admin/Controllers/DisplayController.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using UpsellManagementSystem.Models;
+
+namespace UpsellManagementSystem.Areas.Admin.Controllers
+{
+    public class DisplayController : Controller
+    {
+        public DBEntities _display = new DBEntities();
+
+        // GET: Admin/Display/DisplayProducts
+        public ActionResult DisplayProducts()
+        {
+            return View(_display.Products_174866_MiniProj.ToList());
+        }
+
+        // GET: Admin/Display/DisplayCategories
+        public ActionResult DisplayCategories()
+        {
+            return View(_display.Categories_174866_MiniProj.ToList());
+        }
+
+        public ActionResult DeleteProduct(int? id)
+        {
+            var products = _display.Products_174866_MiniProj.Where(x => x.ProductsId == id).FirstOrDefault();
+
+            return View(products);
+        }
+
+        // Post: Admin/Delete/DeleteProduct/id
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteProduct(int id)
+        {
+            Products_174866_MiniProj product = _display.Products_174866_MiniProj.Where(x => x.ProductsId == id).Single<Products_174866_MiniProj>();
+            _display.Products_174866_MiniProj.Remove(product);
+            _display.SaveChanges();
+            return View();
+        }
+    }
+}
