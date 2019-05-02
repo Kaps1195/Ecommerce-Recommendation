e73b365d74c9d315ab43870fbbc09459f28078bb
kramnani <kramnani@IN-MUM-LNDOT032>
2019-05-02 18:11:43 +0530

    Added Admin Login Controller.

---
+++ UpsellManagementSystem/Areas/Admin/Controllers/AdminLoginController.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using UpsellManagementSystem.Models;
+
+namespace UpsellManagementSystem.Areas.Admin.Controllers
+{
+    public class AdminLoginController : Controller
+    {
+        // GET: Admin/AdminLogin
+        public ActionResult AdminLogin()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult AdminLogin(Admin_174866_MiniProj objAdmin)
+        {
+            if (ModelState.IsValid)
+            {
+                using (DBEntities db = new DBEntities())
+                {
+                    var obj = db.Admin_174866_MiniProj.Where(a => a.AdminEmail.Equals(objAdmin.AdminEmail) && a.AdminPwd.Equals(objAdmin.AdminPwd)).FirstOrDefault();
+                    if (obj != null)
+                    {
+                        Session["AdminEmail"] = obj.AdminEmail.ToString();
+                        return RedirectToAction("DisplayProducts", "Display");
+                    }
+                }
+            }
+            return View(objAdmin);
+        }
+    }
+}
